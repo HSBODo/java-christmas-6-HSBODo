41daa0fe3bac030f5e1618096e9b92b891364b97
HSBODo <vinsulill@gmaile.com>
2023-11-10 18:03:56 +0900

    test : 범위안에 포함된 숫자인지검증하는 메서드 테스트코드 구현
    테스트 메서드
    - isRangeReservationDate_정상케이스()
    - isRangeReservationDate_예외케이스

--- src/test/java/christmas/service/serviceimpl/ValidationServiceImplTest.java
+++ src/test/java/christmas/service/serviceimpl/ValidationServiceImplTest.java
@@ -2,45 +2,83 @@ package christmas.service.serviceimpl;
 
 import christmas.constant.ChristmasPromotionException;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class ValidationServiceImplTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"15"})
-    @DisplayName("입렵 값이 숫자형인지 확인한다.")
+    @DisplayName("입렵 값이 숫자형이면 true를 반환한다")
     public void isDigit_정상케이스(String number) {
-        Object result = null;
+        boolean result = false;
         Exception exception = new Exception();
+
         try {
-            result = Integer.parseInt(number);
+           Integer.parseInt(number);
+            result=true;
         }catch (NumberFormatException e){
             exception = new IllegalArgumentException(ChristmasPromotionException.INPUT_NOT_DiGIT.getMessage());
         }
 
-        assertThat(result).isEqualTo(Integer.parseInt(number));
-        assertThat(exception).isNotInstanceOf(IllegalArgumentException.class)
-                .hasMessageNotContaining("[ERROR] 숫자로 입력해 주세요.");
+        assertThat(result).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"십오"})
     @DisplayName("입렵 값이 숫자형이 아니면 예외를 발생한다.")
     public void isDigit_예외케이스(String number) {
+        boolean result = false;
         Exception exception = new Exception();
+
         try {
             Integer.parseInt(number);
+            result=true;
         }catch (NumberFormatException e){
             exception = new IllegalArgumentException(ChristmasPromotionException.INPUT_NOT_DiGIT.getMessage());
         }
+
+        assertThat(result).isFalse();
         assertThat(exception).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 숫자로 입력해 주세요.");
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,1,31", "5,1,31", "15,1,31", "20,1,31", "31,1,31"}, delimiter = ',')
+    @DisplayName("입렵 값이 범위에 포함되면 true를 반환한다.")
+    public void isRangeReservationDate_정상케이스(int reservationDate,int start,int end) {
+        boolean result = false;
+        Exception exception;
+
+        if(start <= reservationDate && end >= reservationDate){
+            result = true;
+        }
+        exception = new IllegalArgumentException(ChristmasPromotionException.INPUT_NOT_VALID_RANGE.getMessage());
+
+        assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"32,1,31", "0,1,31", "33,1,31", "34,1,31", "100,1,31"}, delimiter = ',')
+    @DisplayName("입렵 값이 숫자형인지 확인한다.")
+    public void isRangeReservationDate_예외케이스(int reservationDate,int start,int end) {
+        boolean result = false;
+        Exception exception;
+
+        if(start <= reservationDate && end >= reservationDate){
+            result = true;
+        }
+        exception = new IllegalArgumentException(ChristmasPromotionException.INPUT_NOT_VALID_RANGE.getMessage());
+
+        assertThat(result).isFalse();
+        assertThat(exception).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ChristmasPromotionException.INPUT_NOT_VALID_RANGE.getMessage());
+    }
+
 }
