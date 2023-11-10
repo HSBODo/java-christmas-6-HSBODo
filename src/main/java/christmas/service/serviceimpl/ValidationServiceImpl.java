26fef6506a61b32565e4777c655a6ee0a8f75a69
HSBODo <vinsulill@gmaile.com>
2023-11-10 18:31:07 +0900

    feat : isReservationDate() 인터페이스 구현

--- src/main/java/christmas/service/serviceimpl/ValidationServiceImpl.java
+++ src/main/java/christmas/service/serviceimpl/ValidationServiceImpl.java
@@ -4,10 +4,19 @@ import christmas.constant.ChristmasPromotionException;
 import christmas.service.ValidationService;
 
 public class ValidationServiceImpl implements ValidationService {
+    private final int RESERVATION_DATE_RANGE_START = 1;
+    private final int RESERVATION_DATE_RANGE_END = 31;
 
     @Override
-    public boolean isReservationDate(String date){
-       return false;
+    public boolean isReservationDate(String reservationDate){
+        try {
+            isDigit(reservationDate);
+            isRangeReservationDate(Integer.parseInt(reservationDate),RESERVATION_DATE_RANGE_START,RESERVATION_DATE_RANGE_END);
+            return true;
+        }catch (Exception e){
+            System.out.println(ChristmasPromotionException.INPUT_NOT_VALID_DATE.getMessage());
+            return false;
+        }
     }
 
     @Override
@@ -25,7 +34,7 @@ public class ValidationServiceImpl implements ValidationService {
     }
 
     private boolean isRangeReservationDate(int reservationDate,int start,int end){
-        if(start <= reservationDate || end >= reservationDate){
+        if(start <= reservationDate && end >= reservationDate){
             return true;
         }
         throw new IllegalArgumentException(ChristmasPromotionException.INPUT_NOT_VALID_RANGE.getMessage());
