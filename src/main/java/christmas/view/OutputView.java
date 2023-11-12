618a58f3c068b4b82a8871f147f10896b098243d
HSBODo <vinsulill@gmaile.com>
2023-11-12 21:08:52 +0900

    feat : OutputView 첫 인사말 메서드 추가
    구현 메서드
    - firstGreeting()

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+public class OutputView {
+    private final String FIRST_GREETING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public void firstGreeting(){
+        System.out.println(FIRST_GREETING);
+    }
+
+}
