38ab66e812f6d6e556d3bcc6ccb452b5a8fa801f
HSBODo <vinsulill@gmaile.com>
2023-11-12 22:10:26 +0900

    feat : 예약정보 Dto 구현
    구현 메서드
    - ReservationInfoDto()
    - initReservationMenus()
    - initTotalPriceBeforeDiscount()

---
+++ src/main/java/christmas/dto/ReservationInfoDto.java
@@ -0,0 +1,43 @@
+package christmas.dto;
+
+import christmas.constant.Menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservationInfoDto {
+    private String reservationDate;
+    private List<Menu> reservationMenus;
+    private int totalPriceBeforeDiscount;
+    private int totalPriceAfterDiscount;
+    private int totalDiscountPrice;
+    private String giveaway;
+    private String badge;
+
+    public ReservationInfoDto(String reservationDate, String reservationMenuAndQuantity) {
+        this.reservationDate = reservationDate;
+        initReservationMenus(reservationMenuAndQuantity);
+        initTotalPriceBeforeDiscount();
+    }
+
+    private void initReservationMenus(String reservationMenuAndQuantity){
+        reservationMenus = new ArrayList<>();
+        String[] reservationMenuAndQuantityCommaSplit = reservationMenuAndQuantity.split(",");
+        for(String menuAndQuantity:reservationMenuAndQuantityCommaSplit){
+            String[] menuAndQuantityHyphenSplit = menuAndQuantity.split("-");
+            reservationMenus.add(Menu.getMenu(menuAndQuantityHyphenSplit[0]));
+        }
+    }
+
+    private void initTotalPriceBeforeDiscount(){
+        reservationMenus.stream().forEach(menu -> {
+            totalPriceBeforeDiscount+=menu.getPrice();
+        });
+
+    }
+
+    public int getTotalPriceBeforeDiscount() {
+        return totalPriceBeforeDiscount;
+    }
+
+}
