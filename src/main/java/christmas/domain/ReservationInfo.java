8cc7cff82dc253b9bd97ed565eb681b949270742
HSBODo <vinsulill@gmaile.com>
2023-11-14 03:04:00 +0900

    feat : Domain to Model 메서드 구현

--- src/main/java/christmas/domain/ReservationInfo.java
+++ src/main/java/christmas/domain/ReservationInfo.java
@@ -30,4 +30,52 @@ public class ReservationInfo {
         this.giveaway = giveaway;
         this.badge = badge;
     }
+
+    public Model toOrderMenuModel(){
+        Model model = new Model();
+        menusQuantity.forEach((menuName, quantity) -> {
+            model.addAttribute(menuName,quantity);
+        });
+        return model;
+    }
+
+    public Model toTotalPriceBeforeDiscountModel(){
+        Model model = new Model();
+        model.addAttribute("totalPriceBeforeDiscount",totalPriceBeforeDiscount);
+        return model;
+    }
+
+    public Model toGiveawayModel(){
+        Model model = new Model();
+        model.addAttribute("giveWay",giveaway);
+        model.addAttribute("quantity",1);
+        return model;
+    }
+
+    public Model toDiscountDetailsModel(){
+        Model model = new Model();
+        discountDetails.forEach((discountTitle, discountPrice) -> {
+            model.addAttribute(discountTitle,discountPrice);
+        });
+        return model;
+    }
+
+    public Model toTotalDiscountPriceModel(){
+        Model model = new Model();
+        model.addAttribute("totalDiscountPrice",totalDiscountPrice);
+        return model;
+    }
+
+    public Model toTotalPriceAfterDiscountModel(){
+        Model model = new Model();
+        model.addAttribute("totalPriceAfterDiscount",totalPriceAfterDiscount);
+        return model;
+    }
+
+    public Model toBadgeModel(){
+        Model model = new Model();
+        model.addAttribute("badge",badge);
+        return model;
+    }
+
 }
