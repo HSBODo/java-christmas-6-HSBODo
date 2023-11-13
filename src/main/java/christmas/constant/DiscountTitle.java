a46842d42508f84973f0cde780c4352c4ebf8685
HSBODo <vinsulill@gmaile.com>
2023-11-14 03:33:59 +0900

    feat : DiscountTitle title 추가

--- src/main/java/christmas/constant/DiscountTitle.java
+++ src/main/java/christmas/constant/DiscountTitle.java
@@ -1,5 +1,19 @@
 package christmas.constant;
 
 public enum DiscountTitle {
-    크리스마스_디데이_할인,평일_할인,주말_할인,특별_할인,증정_이벤트
+    크리스마스_디데이_할인("크리스마스 디데이 할인"),
+    평일_할인("평일 할인"),
+    주말_할인("주말 할인"),
+    특별_할인("특별 할인"),
+    증정_이벤트("증정 이벤트");
+
+    final private String title;
+
+    DiscountTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
 }
