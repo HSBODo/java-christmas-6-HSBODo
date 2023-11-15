4e3ce078467ce74666666557991ea2f7bb6d115d
HSBODo <vinsulill@gmaile.com>
2023-11-15 18:52:46 +0900

    feat : 배지 상수 enum 클래스로 관리

---
+++ src/main/java/christmas/constant/Badge.java
@@ -0,0 +1,39 @@
+package christmas.constant;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum Badge {
+    SANTA("산타",-20000),
+    TREE("트리",-10000),
+    START("별",-5000),
+    EMPTY("없음",0),
+    ;
+
+    private static final int BADGE_INDEX = 0;
+
+    private final String name;
+    private final int moreThanOrEqualTo;
+
+    Badge(String name, int moreThanOrEqualTo) {
+        this.name = name;
+        this.moreThanOrEqualTo = moreThanOrEqualTo;
+    }
+
+    public static Badge getBadge(int totalBenefitsPrice) {
+        List<Badge> badges = Arrays.stream(Badge.values()).filter(badge ->
+                        badge.getMoreThanOrEqualTo() >= totalBenefitsPrice
+                ).collect(Collectors.toList());
+
+        return badges.get(BADGE_INDEX);
+    }
+
+    public  String getName() {
+        return name;
+    }
+
+    public int getMoreThanOrEqualTo() {
+        return moreThanOrEqualTo;
+    }
+}
