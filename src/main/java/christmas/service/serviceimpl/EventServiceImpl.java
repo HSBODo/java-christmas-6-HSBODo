86d8b4d25d7b64eb8bdb8385506ffb73fd3ba1ad
HSBODo <vinsulill@gmaile.com>
2023-11-15 18:56:59 +0900

    refactor : 각각의 이벤트 메서드의 조건과 적용 역할 분리

--- src/main/java/christmas/service/serviceimpl/EventServiceImpl.java
+++ src/main/java/christmas/service/serviceimpl/EventServiceImpl.java
@@ -1,15 +1,13 @@
 package christmas.service.serviceimpl;
 
-import christmas.constant.BenefitsTitle;
-import christmas.constant.Menu;
+import christmas.constant.DayOfTheWeek;
+import christmas.domain.event.*;
 import christmas.dto.ReservationInfoDto;
 import christmas.service.EventService;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+
 
 public class EventServiceImpl implements EventService {
     private final int EVENT_APPLY_NECESSARY_PRICE = 10000;
@@ -32,136 +30,69 @@ public class EventServiceImpl implements EventService {
 
     private boolean isApplyEvent(ReservationInfoDto reservationInfoDto){
         int totalPriceBeforeDiscount = reservationInfoDto.getTotalPriceBeforeDiscount();
+
         if(totalPriceBeforeDiscount<EVENT_APPLY_NECESSARY_PRICE) return false;
+
         return true;
     }
 
-    private ReservationInfoDto applyChristmasD_DayDiscount(ReservationInfoDto reservationInfoDto){
-        final BenefitsTitle benefitsTitle = BenefitsTitle.크리스마스_디데이_할인;
-        final int oneDayPerDiscountPrice = 100;
+    private void applyChristmasD_DayDiscount(ReservationInfoDto reservationInfoDto){
         final int discountFirstDay = 1;
         final int discountLastDay = 25;
 
-        int minDiscountPrice = 1000;
         int reservationDay = reservationInfoDto.getReservationDay();
+        //적용조건
+        if(reservationDay > discountLastDay || reservationDay < discountFirstDay) return;
 
-        if(reservationDay > discountLastDay || reservationDay < discountFirstDay) return reservationInfoDto;
-
-        int discountPrice = minDiscountPrice + ((reservationDay-1) * oneDayPerDiscountPrice);
-        reservationInfoDto.applyDiscountPrice(benefitsTitle, discountPrice);
-
-        return reservationInfoDto;
+        new ChristmasDdayDiscount().apply(reservationInfoDto);
     }
 
-    private ReservationInfoDto applyWeekdayDiscount(ReservationInfoDto reservationInfoDto){
-        final BenefitsTitle benefitsTitle = BenefitsTitle.평일_할인;
-        final int discountCategoryPerDiscountPrice = 2023;
-        final String discountCategory = "디저트";
-
-        Map<Menu, Integer> reservationMenusQuantity = reservationInfoDto.getReservationMenusQuantity();
+    private void applyWeekdayDiscount(ReservationInfoDto reservationInfoDto){
         int reservationDay = reservationInfoDto.getReservationDay();
-        int dessertQuantity = 0;
-
-        if(!isWeekday(reservationDay)) return reservationInfoDto;
-
-        for (Menu menu: reservationMenusQuantity.keySet()){
-            if (menu.getCategory()==discountCategory){
-                dessertQuantity += reservationMenusQuantity.get(menu);
-            }
-        }
-        int discountPrice = dessertQuantity * discountCategoryPerDiscountPrice;
-        reservationInfoDto.applyDiscountPrice(benefitsTitle, discountPrice);
-        return reservationInfoDto;
+        //적용조건
+        if(isWeekday(reservationDay)) new WeekdayDiscount().apply(reservationInfoDto);
     }
 
-    private ReservationInfoDto applyWeekendDiscount(ReservationInfoDto reservationInfoDto){
-        final BenefitsTitle benefitsTitle = BenefitsTitle.주말_할인;
-        final String discountCategory = "메인";
-        final int discountCategoryPerDiscountPrice = 2023;
-
-        Map<Menu, Integer> reservationMenusQuantity = reservationInfoDto.getReservationMenusQuantity();
+    private void applyWeekendDiscount(ReservationInfoDto reservationInfoDto){
         int reservationDay = reservationInfoDto.getReservationDay();
-        int mainQuantity = 0;
-
-        if(!isWeekend(reservationDay)) return reservationInfoDto;
-
-        for (Menu menu: reservationMenusQuantity.keySet()){
-            if (menu.getCategory()==discountCategory){
-                mainQuantity += reservationMenusQuantity.get(menu);
-            }
-        }
-        int discountPrice = mainQuantity * discountCategoryPerDiscountPrice;
-        reservationInfoDto.applyDiscountPrice(benefitsTitle, discountPrice);
-        return reservationInfoDto;
+        //적용조건
+        if(isWeekend(reservationDay)) new WeekendDiscount().apply(reservationInfoDto);
     }
 
-    private ReservationInfoDto giveawayEvent(ReservationInfoDto reservationInfoDto){
-        final BenefitsTitle benefitsTitle = BenefitsTitle.증정_이벤트;
-        final Menu giveawayMenu = Menu.CHAMPAGNE;
-        final int giveawayQuantity = 1;
-        final int giveawayEventNecessaryPrice = 120000;
-
+    private void giveawayEvent(ReservationInfoDto reservationInfoDto){
+        final int giveawayEventNecessaryTotalPriceBeforeDiscount = 120000;
         int totalPriceBeforeDiscount = reservationInfoDto.getTotalPriceBeforeDiscount();
-
-        if(totalPriceBeforeDiscount>=giveawayEventNecessaryPrice){
-            reservationInfoDto.applyGiveaway(benefitsTitle,giveawayMenu,giveawayQuantity);
-            return reservationInfoDto;
-        }
-        return reservationInfoDto;
+        //적용조건
+        if(totalPriceBeforeDiscount>=giveawayEventNecessaryTotalPriceBeforeDiscount) new GiveawayEvent().apply(reservationInfoDto);
     }
 
-    private ReservationInfoDto specialDiscount(ReservationInfoDto reservationInfoDto){
-        final BenefitsTitle benefitsTitle = BenefitsTitle.특별_할인;
-        final List<Integer> specialDay = new ArrayList<>(List.of(3,10,17,24,25,31));
-        final int discountPrice = 1000;
-
+    private void specialDiscount(ReservationInfoDto reservationInfoDto){
+        SpecialDiscount specialDiscount = new SpecialDiscount();
         int reservationDay = reservationInfoDto.getReservationDay();
-
-        if(specialDay.contains(reservationDay)){
-            reservationInfoDto.applyDiscountPrice(benefitsTitle, discountPrice);
-            return reservationInfoDto;
-        }
-        return reservationInfoDto;
+        //적용조건
+        if(specialDiscount.isSpecialDay(reservationDay)) specialDiscount.apply(reservationInfoDto);
     }
 
-    private ReservationInfoDto badgeEvent(ReservationInfoDto reservationInfoDto){
-        int totalBenefitsPrice = reservationInfoDto.getTotalBenefitsPrice()*-1;
-        System.out.println("totalBenefitsPrice = " + totalBenefitsPrice);
-        if(totalBenefitsPrice>=20000){
-            reservationInfoDto.setBadge("산타");
-            return reservationInfoDto;
-        }
-        if(totalBenefitsPrice>=10000){
-            reservationInfoDto.setBadge("트리");
-            return reservationInfoDto;
-        }
-        if(totalBenefitsPrice>=5000) {
-            reservationInfoDto.setBadge("별");
-            return reservationInfoDto;
-        }
-        return reservationInfoDto;
+    private void badgeEvent(ReservationInfoDto reservationInfoDto){
+        new BadgeEvent().apply(reservationInfoDto);
     }
 
-    private int findDayOfWeek(int year, int month, int day){
-        //월요일(1)~일요일(7)
+    private DayOfTheWeek findDayOfWeek(int year, int month, int day){
         LocalDate date = LocalDate.of(year, month, day);
         DayOfWeek dayOfWeek = date.getDayOfWeek();
-        return dayOfWeek.getValue();
+        DayOfTheWeek dayOfTheWeek = DayOfTheWeek.getDayOfTheWeek(dayOfWeek.getValue());
+        return dayOfTheWeek;
     }
 
     private boolean isWeekday(int day){
-        int dayOfWeek = findDayOfWeek(EVENT_APPLY_YEAR, EVENT_APPLY_MONTH, day);
-        if(dayOfWeek == christmas.constant.DayOfWeek.금요일.getValue() || dayOfWeek ==  christmas.constant.DayOfWeek.토요일.getValue()){
-            return false;
-        }
-        return true;
+        DayOfTheWeek dayOfWeek = findDayOfWeek(EVENT_APPLY_YEAR, EVENT_APPLY_MONTH, day);
+        if(dayOfWeek.getCategory() == "평일") return true;
+        return false;
     }
 
     private boolean isWeekend(int day){
-        int dayOfWeek = findDayOfWeek(EVENT_APPLY_YEAR, EVENT_APPLY_MONTH,day);
-        if(dayOfWeek == christmas.constant.DayOfWeek.금요일.getValue() || dayOfWeek ==  christmas.constant.DayOfWeek.토요일.getValue()){
-            return true;
-        }
+        DayOfTheWeek dayOfWeek = findDayOfWeek(EVENT_APPLY_YEAR, EVENT_APPLY_MONTH, day);
+        if(dayOfWeek.getCategory() == "주말") return true;
         return false;
     }
 
